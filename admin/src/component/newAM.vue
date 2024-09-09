50eac39ff608b337cebb29aeccc6b8cb4e10f55a
apricityx <apricityx@qq.com>
2024-09-10 00:21:33 +0800

    Dev. 新建作业前端表单完成

--- admin/src/component/newAM.vue
+++ admin/src/component/newAM.vue
@@ -3,41 +3,113 @@ import {Config} from "@/loadConfig";
 import {ref} from "vue";
 import {stdInfo} from "@/requests/getStdInfo";
 import type {Assignment} from "@/requests/getAM"
-import type { StdInfo } from "@/requests/getStdInfo"
+import type {StdInfo} from "@/requests/getStdInfo"
+import {tr} from "vuetify/locale";
 
 // 表单验证
-const valid = ref(true)
+const valid = ref(false)
 const validRule = ref({
   nameRules: [
     (value: string) => {
-      return !!value || 'Name is required'
+      if (value.length > 0) {
+        return true
+      } else {
+        return '作业名不能为空'
+      }
+    }
+  ],
+  monthRules: [
+    (value: number) => {
+      if (Form.value.AMDeadlineYear > timeNow.getFullYear()) {
+        return true
+      } else if (value > timeNow.getMonth()) {
+        return true
+      }
+      return "不可设置为过去的时间"
+    }
+  ],
+  dayRules: [
+    (value: number) => {
+      if (Form.value.AMDeadlineMonth === timeNow.getMonth() + 1) {
+        if (value >= timeNow.getDate()) {
+          return true
+        } else {
+          return "不可设置为过去的时间"
+        }
+      } else {
+        return true
+      }
+    }
+  ],
+  hourRules: [
+    (value: number) => {
+      if (Form.value.AMDeadlineDay === timeNow.getDate()) {
+        if (value > timeNow.getHours()) {
+          return true
+        } else {
+          return "不可设置为过去的时间"
+        }
+      } else {
+        return true
+      }
+    }
+  ],
+  DescriptionRules: [
+    (value: string) => {
+      return !!value || 'Description is required'
     }
   ]
 })
+
 const Form = ref({
-  name: '',
-  lastname: '',
-  email: ''
+  AMName: '',
+  AMDeadlineYear: 2024,
+  AMDeadlineMonth: 1,
+  AMDeadlineDay: 1,
+  AMDescription: '',
+  AMDeadlineHour: 1,
 })
-Form.value.name = stdInfo.name
+// 提交表单
+const submitAM = () => {
+
+}
+
+const timeNow = new Date()
+Form.value.AMDeadlineYear = timeNow.getFullYear()
+Form.value.AMDeadlineMonth = timeNow.getMonth() + 1
+Form.value.AMDeadlineDay = timeNow.getDate()
+Form.value.AMDeadlineHour = timeNow.getHours() === 23 ? 23 : timeNow.getHours() + 1
+const generateDay = () => {
+  const month = Form.value.AMDeadlineMonth
+  if (month === 2) {
+    if (Form.value.AMDeadlineYear % 4 === 0) {
+      return 29
+    } else {
+      return 28
+    }
+  } else if ([1, 3, 5, 7, 8, 10, 12].includes(month)) {
+    return 31
+  } else {
+    return 30
+  }
+}
 </script>
 
 <template>
   服务器地址：{{ Config.beServerAddress }}
   <v-card
       class="mx-auto"
-      prepend-icon="mdi-upload"
-      subtitle="新建作业"
+      prepend-icon="mdi-plus-box"
       width="80%"
   >
     <template v-slot:title>
-      <span class="font-weight-black">上传文件</span>
+      <span class="font-weight-black">新建作业</span>
     </template>
 
     <v-card-text class="bg-surface-light pt-4">
       <v-card-item>
         <template v-slot:subtitle>
-          <v-container v-show="">
+          <v-container v-show="true">
             <v-icon
                 class="me-1 pb-1"
                 color="error"
@@ -50,49 +122,70 @@ Form.value.name = stdInfo.name
 
           <v-card-item>
             <v-container>
-              <v-row>
-                <v-col
-                    cols="12"
-                    md="4"
-                >
-                  <v-text-field
-                      v-model="Form.name"
-                      :counter="10"
-                      :rules="validRule.nameRules"
-                      label="姓名"
-                      hide-details
-                      required
-                      disabled
-                  ></v-text-field>
-                </v-col>
+              <v-form>
+                <v-row>
+                  <v-col
+                      cols="12"
+                      md="4"
+                  >
+                    <v-text-field
+                        v-model="Form.AMName"
+                        :counter="10"
+                        :rules="validRule.nameRules"
+                        label="作业名"
+                        hide-details
+                        required
+                    ></v-text-field>
+                  </v-col>
 
-                <v-col
-                    cols="12"
-                    md="4"
-                >
-                  <v-text-field
-                      v-model="lastname"
-                      :counter="10"
-                      :rules="nameRules"
-                      label="Last name"
-                      hide-details
-                      required
-                  ></v-text-field>
-                </v-col>
+                  <v-col
+                      cols="12"
+                      md="4"
+                  >
+                    <v-select
+                        v-model="Form.AMDeadlineYear"
+                        label="年"
+                        :items="Array.from({length: 10}, (v, k) => timeNow.getFullYear() + k)"
+                    />
+                    <v-select
+                        v-model="Form.AMDeadlineMonth"
+                        label="月"
+                        :items="Array.from({length: 12}, (v, k) => 1 + k)"
+                        :rules="validRule.monthRules"
+                    />
+                    <v-select
+                        v-model="Form.AMDeadlineDay"
+                        label="日"
+                        :items="Array.from({length: generateDay()}, (v, k) => 1 + k)"
+                        :rules="validRule.dayRules"
+                    />
+                    <v-select
+                        v-model="Form.AMDeadlineHour"
+                        label="时"
+                        :items="Array.from({length:24}, (v, k) => k)"
+                        :rules="validRule.hourRules"
+                    />
 
-                <v-col
-                    cols="12"
-                    md="4"
-                >
-                  <v-text-field
-                      v-model="email"
-                      :rules="emailRules"
-                      label="E-mail"
-                      hide-details
-                      required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
+                  </v-col>
+
+                  <v-col
+                      cols="12"
+                      md="4"
+                  >
+                    <v-textarea
+                        label="作业详情"
+                        v-model="Form.AMDescription"
+                        auto-grow
+                    >
+                    </v-textarea>
+                  </v-col>
+                </v-row>
+                <v-btn
+                    @click="submitAM"
+                    block
+                    text="提交"
+                />
+              </v-form>
             </v-container>
           </v-card-item>
         </template>
